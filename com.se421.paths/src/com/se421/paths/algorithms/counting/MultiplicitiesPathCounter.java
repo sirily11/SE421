87b8b53e310d4a66659887fba4b09938862a00de
sirily11 <sirily1997@gmail.com>
2019-10-26 21:44:42 -0500

    Finish step 2

--- com.se421.paths/src/com/se421/paths/algorithms/counting/MultiplicitiesPathCounter.java
+++ com.se421.paths/src/com/se421/paths/algorithms/counting/MultiplicitiesPathCounter.java
@@ -1,5 +1,8 @@
 package com.se421.paths.algorithms.counting;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 import com.ensoftcorp.atlas.core.db.graph.Edge;
 import com.ensoftcorp.atlas.core.db.graph.Node;
 import com.ensoftcorp.atlas.core.db.set.AtlasSet;
@@ -36,16 +39,75 @@ public class MultiplicitiesPathCounter extends PathCounter {
 		// and the number of additions required to count the path
 		long numPaths = 0;
 		long additions = 0;
-
+		
+		HashMap<Node, Long> multiplicities = new HashMap<Node, Long>();
+		HashMap<Node, Long> triggers = new HashMap<Node, Long>();
 		// create a directed acyclic graph (DAG)
 		DAGTransform transformer = new DAGTransform();
 		Q dag = transformer.transform(cfg);
-		
 		// the roots and leaves of the DAG
 		AtlasSet<Node> dagLeaves = dag.leaves().eval().nodes();
 		Node dagRoot = dag.roots().eval().nodes().one();
 
-		// TODO: implement a O(n) solution to count paths
+		Stack<Node> stack = new Stack<Node>();
+
+		// start searching from the root
+		stack.push(dagRoot);
+		// depth first search on directed acyclic graph
+		while (!stack.isEmpty()) {
+			// next node to process
+			Node currentNode = stack.pop();
+		
+			// get the children of the current node
+			// note: we iterate by edge in case there are multiple edges from a predecessor to a successor
+			for (Edge outgoingEdge : dag.forwardStep(Common.toQ(currentNode)).eval().edges()) {
+				Node successor = outgoingEdge.to();
+				Long ms;
+				Long ts;
+				Long mv;
+				// Number of edges from v to s
+				Long numEdges = dag.betweenStep(Common.toQ(currentNode), Common.toQ(successor)).eval().edges().size();
+				
+				if(multiplicities.containsKey(successor)) {
+					ms = multiplicities.get(successor);
+				} else {
+					ms = (long) 0;
+				}
+				
+				if(multiplicities.containsKey(currentNode)) {
+					mv = multiplicities.get(currentNode);
+					
+				} else {
+					if(currentNode.equals(dagRoot)) {
+						mv = (long) 1;
+					} else {
+						mv = (long) 0;
+					}
+				}
+				
+				if(triggers.containsKey(successor)) {
+					ts = triggers.get(successor);
+				} else {
+					ts = dag.reverseStep(Common.toQ(successor)).eval().edges().size();
+				}
+				
+				ms = ms + numEdges * mv;
+				ts = ts - numEdges;
+				triggers.put(successor, ts);
+				multiplicities.put(successor, ms);
+				Log.info("MS: " + ms + " NumEdges: " + numEdges + " MV: " + mv + " TS: " + ts);
+				if(ts == 0) {
+					if(dagLeaves.contains(successor)) {
+						// if we reached a leaf increment the counter by 1
+						numPaths = numPaths + ms;
+						additions++;
+					} else {
+						// push the child node on the stack to be processed
+						stack.push(successor);
+					}
+				}			
+			}
+		}
 		
 		// at the end, we have traversed all paths once, so return the count
 		return new CountingResult(additions, numPaths);
